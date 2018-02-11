28d9c8c60059b966becae4f2afa908aeb6d62e2b
ganzhen <ganzhen@wangfeis-MacBook-Air.local>
2018-02-11 13:51:59 +0800

    fix

--- stockcrawler/src/main/java/com/github/springcloud/stockcrawler/pageenity/StockStarComStockCodesPageEntity.java
+++
@@ -1,7 +0,0 @@
-package com.github.springcloud.stockcrawler.pageenity;
-
-/**
- * 证券之星网站的股票全部代码页面实体类，对应地址（http://quote.stockstar.com/stock/stock_index.htm）
- */
-public class StockStarComStockCodesPageEntity {
-}
