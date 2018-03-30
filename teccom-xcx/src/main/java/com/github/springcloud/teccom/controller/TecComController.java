6c86a708e5d5801e286a19959418d5b825364741
ganzhen <ganzhen@wangfeis-MacBook-Air.local>
2018-03-30 17:18:47 +0800

    fix

--- teccom-xcx/src/main/java/com/github/springcloud/teccom/controller/TecComController.java
+++ teccom-xcx/src/main/java/com/github/springcloud/teccom/controller/TecComController.java
@@ -1,15 +1,15 @@
 package com.github.springcloud.teccom.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.github.springcloud.teccom.service.TecComService;
 import com.github.springcloud.teccom.vo.ResultVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * Created by ganzhen on 26/03/2018.
@@ -30,5 +30,11 @@ public class TecComController {
         logger.info("in TecComController.installRequestApply: customerName="+customerName+",customerPhone="+customerPhone+",customerAddress="+customerAddress);
         ResultVo vo = tecComServiceImpl.saveCustomerInstallBroadBandRequest(customerName,customerPhone,customerAddress);
         return ResponseEntity.ok(vo);
+    }
+
+    @RequestMapping(value="postTest",method = RequestMethod.POST)
+    public ResponseEntity<?> postTest(@RequestBody JSONObject jo){
+        logger.info(jo.toJSONString());
+        return ResponseEntity.ok(true);
     }
 }
